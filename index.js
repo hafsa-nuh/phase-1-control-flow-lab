395631233632312ad78567c382949d10516db440
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-08-14 18:26:14 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(customers){
   // Write your code here!
+  if (customers  <= 400){
+    return "This one is on me!"
+  }else if ( customers >= 2000 && customers <= 2499){
+    return "I will gladly take your thirty bucks."
+  }else  {
+    return 'No can do.'
+  } 
+}
+ 
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === 'NYC' ? "Ok, sounds good." :  "No go." 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case "generous":
+      return 'Thank you so much.'
+      break;
+    case "not as generous":
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.'
+  } 
 }
